c931e6b8d421e41f03d6891d245352061052f49c
awayal <1130120198@qq.com>
2021-12-16 12:23:20 +0800

    代码整理

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,14 @@
-const input = document.createElement('input');
-input.type="file";
-document.body.appendChild(input);
-
 btn.onclick = () => {
     const formData = new FormData();
     formData.append("userName", "kingKKKKK");
     formData.append("upwd", "123456798");
     formData.append("age", "18");
-    formData.append("file", input.files[0])
     fetch("http://localhost:8000/api/post", {
         method: "POST",
         body: formData,
         // body: JSON.stringify({
         //     userName: "123456",
-        //     name: "张三"
+        //     name: "keqing"
         // }),
         // headers: {
         //     "content-type": "application/json"
